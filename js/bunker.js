b2bb89b1e27b26f6dc5982b3fb8622f3aa4f77e5
SBandoli <77977329+Bertania@users.noreply.github.com>
2021-11-25 19:15:45 -0600

    [finishes #180439752] Add music

--- js/bunker.js
+++ js/bunker.js
@@ -3,12 +3,18 @@ var difficultySelector;
 $(function() {
     
     $('.bunker').hide();
+
+    var score = 0;
+    var bgMusic = new Audio('media/bunkerBg.mp3');
+    bgMusic.loop = true;
     
     $('#diff_easy').on("click", function() {
 
         var difficultySelector = "easy";
     
         $('.difficulty').hide();
+
+        bgMusic.play();
         
         $('.bunker').show();
     });
@@ -18,6 +24,8 @@ $(function() {
     var difficultySelector = "normal";
     
     $('.difficulty').hide();
+
+    bgMusic.play();
         
     $('.bunker').show();
     });
@@ -27,18 +35,37 @@ $(function() {
     var difficultySelector = "hard";
     
     $('.difficulty').hide();
+
+    bgMusic.play();
         
     $('.bunker').show();
     });
 
   $('#b_look').on("click", function() {
 
-    $("#b_dialogue").html("<p>Looks like there is a key on the floor.</p>"); 
+    $("#b_dialogue").html("<p>Still the same old grimey bunker.  The radio is still on.  Looks like I left a key on the floor too.</p>"); 
+    $('#b_music').css("visibility", "visible");
+  });
+
+  // Toggling background music on and off
+  $('#b_music').on("click", function() {
+
+    if (!bgMusic.paused) { // Music is playing
+      bgMusic.pause();
+      $("#b_music").text("Turn radio on")
+    } else {  //  Music isn't playing
+      bgMusic.play();
+      $("#b_music").text("Turn radio off")
+    }
+
+    $("b_dialogue").html("<p>I hated that song.</p>");
   });
 
   $('#b_item').on("click", function() {
 
-    $("#b_dialogue").html("<p>I could pick it up...it's kinda far though..</p>"); 
+    $("#b_dialogue").html("<p>I could pick it up...it's kinda far though..</p>");
+    
+    var score =+ 10
   });
 
   $('#b_stare').on("click", function() {
