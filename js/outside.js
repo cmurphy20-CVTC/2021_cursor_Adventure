d71a9359193083734f93424fd8a9d54887947673
Zebra <jkenner2@student.cvtc.edu>
2021-12-02 11:18:58 -0600

    [finishes #180509692] Remove the key and display inventory outside bunker

--- js/outside.js
+++ js/outside.js
@@ -1,4 +1,15 @@
 $(function() {
+    // Reset inventory to remove key 
+    var inventory = ["Bottle of water", "Food ration"];
+
+    // Display inventory header and number of items
+    $("#inventory").html("Current Inventory(" + inventory.length + ")");
+
+    // Display current inventory items 
+    inventory.forEach(function (item) {
+      $("#inventory_items").append("<li>" + item + "</li>");
+    });
+    
     $('#o_look').on("click", function() {
 
     $("#o_dialogue").html("<p>Testing Outside Dialogue Box.</p>"); 
