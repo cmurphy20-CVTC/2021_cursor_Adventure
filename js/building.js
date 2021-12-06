f207f349df1ff9001e4d6dccf6fc1e41bc6ac096
SBandoli <77977329+Bertania@users.noreply.github.com>
2021-12-05 23:22:58 -0600

    [#Finishes, Delivers #180486884], [#Finishes, Delivers #180509094] Added combat sequence to outside js

--- js/building.js
+++ js/building.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 
   var inventory = ["Bottle of water", "Food ration"];
-  var score;
+  var difficulty = localStorage.getItem("difficulty");
+  var score = parseInt(localStorage.getItem("score"));
 
-  $('#o_test').css("display", "none");
+  $('#bd_combat').css("display", "none");
 
   $("#inventory").html("Current Inventory(" + inventory.length + ")");
 
@@ -80,7 +81,7 @@ $(document).ready(function(){
         
               $("#inventory").html("Current Inventory(" + inventory.length + ")");
                 
-              // Adding knif to list of inventory items
+              // Adding knife to list of inventory items
               $("#inventory_items").append("<li>" + inventory[3] + "</li>");
             }
             
@@ -92,7 +93,8 @@ $(document).ready(function(){
 
               $("#bd_dialogue").html("<p>Looks like I wasn't alone...Raiders and it doesn't look like they want to talk...</p>");
 
-              $('#o_test').css("display", "inline-block");
+              $('#bd_combat').css("display", "inline-block");
+              $('#bd_combat').css("visibility", "visible");
 
               $("#bd_itemPick").css("display", "none");
 
@@ -125,7 +127,7 @@ $(document).ready(function(){
 
         $("#bd_dialogue").html("<p>A couple Radiers...guess I get to use my knife sooner that I thought...</p>");
 
-        $('#o_test').css("display", "inline-block");
+        $('#bd_combat').css("display", "inline-block");
 
       });
 
@@ -141,7 +143,180 @@ $(document).ready(function(){
     $("#bd_dialogue").html("<p>\"Furiously swings around knife dispatching imaginary foes\".<p>");
 
   });
+
+  //
+  // Combat sequence
+  //
+  $('#bd_combat').on("click", function() {
+
+    // Hide start and look around button
+    $('#bd_combat').hide();
+    $('#bd_look').hide();
+  
+    // Variables
+    var playerHealth = 50;
+    var playerAttack;
+    var enemyAttack;
+    var minRoll = 1;
+    var maxRoll = 6;
+    var damage;
+    var playerAttackMsg = "";
+    var enemyAttackMsg = "";
+
+    // Function for simulating a standard 1-6 dice roll
+    function attackDmg(minRoll, maxRoll) {
+      return Math.floor(Math.random() * (maxRoll - minRoll)) + minRoll;
+    }
+  
+    // Setting damage types and enemy health for selected difficulty
+    if (difficulty == "easy") {
+
+      // Checking if player has gun before the combat sequence
+      // If they do, they have higher damage
+      if (inventory.length == 4) {
+        var lowDmg = 12;
+        var baseDmg = 14;
+        var critDmg = 16;
+      } else {
+      var lowDmg = 8;
+      var baseDmg = 12;
+      var critDmg = 16;
+      }
+      var enemyHealth = 40;
+  
+    } else if (difficulty == "normal") {
+
+      // Checking if player has gun before the combat sequence
+      // If they do, they have higher damage
+      if (inventory.length == 4) {
+        var lowDmg = 10;
+        var baseDmg = 12;
+        var critDmg = 14;
+      } else {
+      var lowDmg = 6;
+      var baseDmg = 8;
+      var critDmg = 10;
+      }
+      var enemyHealth = 60;
+  
+    } else if (difficulty == "hard") {
+
+      // Checking if player has gun before the combat sequence
+      // If they do, they have higher damage
+      if (inventory.length == 4) {
+        var lowDmg = 8;
+        var baseDmg = 10;
+        var critDmg = 12;
+      } else {
+      var lowDmg = 4;
+      var baseDmg = 6;
+      var critDmg = 8;
+      }
+      var enemyHealth = 70;
+  
+    }
+  
+    $("#bd_dialogue").html("<p>A group of raiders emerge from the stairs.<br>  They look ready to attack you with their melee weapons.<br>  You should attempt to defend yourself.</p>");
+    
+    // Hide pickup item and use item buttons
+    $('#bd_itemPick').hide();
+    $('#bd_useItem').hide();
+  
+    // Show new button and health
+    $('#bd_health').css("visibility", "visible");
+    $('#bd_attack').css("visibility", "visible");
+  
+      $('#bd_attack').on("click", function() {
+  
+      //  Loop for while the enemy or player is alive
+      if (enemyHealth > 1 && playerHealth > 1) {
+  
+        playerAttack = attackDmg(minRoll, maxRoll);
+  
+        //  Player rolls
+        // rolled a 2 or lower
+        if (playerAttack <= 2) {
+          damage = lowDmg;
+          enemyHealth -= damage;
+          score = score + 5;
+          playerAttackMsg = "<p>You attack for " + damage + " points of damage.</p>";
+        
+        //  rolled between 3 and 5
+        } else if (playerAttack == 3 || playerAttack == 4 || playerAttack == 5) {
+          damage = baseDmg;
+          enemyHealth -= damage;
+          score = score + 10;
+          playerAttackMsg = "<p>You attack for " + damage + " points of damage.</p>";
+  
+        //  rolled a 6
+        } else if (playerAttack == 6) {
+          damage = critDmg;
+          enemyHealth -= damage;
+          score = score + 15;
+          playerAttackMsg = "<p>You attack for " + damage + " points of damage.</p>";
+
+        }
+  
+        enemyAttack = attackDmg(minRoll, maxRoll);
+  
+        // Enemy Rolls
+        //  rolled a 2 or lower
+        if (enemyAttack <= 2) {
+          damage = lowDmg;
+          playerHealth = playerHealth - damage;
+          enemyAttackMsg = "<p>You're hit for " + damage + " points of damage.</p>";
+  
+        //  rolled between 3 and 5
+        } else if (enemyAttack == 3 || enemyAttack == 4 || enemyAttack == 5) {
+          damage = baseDmg;
+          playerHealth = playerHealth - damage;
+          enemyAttackMsg = "<p>You're hit for " + damage + " points of damage.</p>";
+          
+        //  rolled a 6
+        } else if (enemyAttack == 6) {
+          damage = critDmg;
+          playerHealth = playerHealth - damage;
+          enemyAttackMsg = "<p>You're hit for " + damage + " points of damage.</p>";
+
+        }
+  
+        $("#bd_dialogue").html(playerAttackMsg + enemyAttackMsg);
+        $("#bd_health").html("Health: " + playerHealth);
+
+        if (playerHealth <= 0 || enemyHealth <= 0) {
+          $("#bd_attack").text("End combat");
+        }
+  
+    } else if (playerHealth <= 0) {
+  
+        $("#bd_dialogue").html("<p>Looks like you died.  Better luck next time.</p>");
+        $('#bd_restart').css("visibility", "visible");
+        $('#bd_attack').css("visibility", "hidden");
+  
+    } else if (enemyHealth <= 0) {
+  
+      score = score + 20;
+  
+        $("#bd_dialogue").html("<p>Looks like I got them all.  That was a close one.</p>");
+  
+        playerHealth = 50;
+  
+      //  Hide health, attack and start combat buttons
+      $('#bd_health').hide();
+      $('#bd_attack').hide();
+    
+      // Show look, pickup item, and use items button
+      $('#bd_look').show();
+      $('#bd_itemPick').show();
+      $('#bd_useItem').show();
+  
+    }
+  
+    // End of attack button function
+  });
+    
+    // End of combat sequence function
+    });
        
   });
   
-
