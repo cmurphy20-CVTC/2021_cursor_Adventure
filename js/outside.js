611a3508b0409a0fefd5d166c2141c96b83ff419
Zebra <jkenner2@student.cvtc.edu>
2021-12-06 12:45:00 -0600

    [finishes #180366393] Take radiation damage after leaving bunker

--- js/outside.js
+++ js/outside.js
@@ -1,4 +1,22 @@
 $(function() {
+    // Set initial health
+    var health = sessionStorage.getItem("healthLevel");
+    console.log(health);
+    $("#o_health").html("Health: " + health);
+
+    // Start method that subtracts radiation damage every 7 seconds
+    setInterval(dmg, 7000);
+
+    // Function used to subtract radiation damage
+    function dmg() {
+        health--;
+        if (health < 0) {
+            alert("You died!")
+            window.location = "1_bunker.html"; 
+        };
+        $("#o_health").html("Health: " + health);
+    }
+
     // Reset inventory to remove key 
     var inventory = ["Bottle of water", "Food ration"];
 
@@ -13,5 +31,7 @@ $(function() {
     $('#o_look').on("click", function() {
 
     $("#o_dialogue").html("<p>Testing Outside Dialogue Box.</p>"); 
+    
+    
   });
 });
